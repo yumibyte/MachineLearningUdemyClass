d62db828c86c9348fb51cfa5308f3f2c0706c1ef
Yumibyte <ashraigosa@gmail.com>
2019-10-21 20:04:45 -0700

    visualizing the predicted data

--- my_simple_linear_regression.R
+++ my_simple_linear_regression.R
@@ -30,3 +30,15 @@ ggplot() +
   ggtitle('Salary vs Experience (Training set)') +
   xlab('Years of expereince') +
   ylab('Salary')
+
+# Visualizing the test set results
+install.packages('ggplot2') 
+library(ggplot2)
+ggplot() +
+  geom_point(aes(x = test_set$YearsExperience, y = test_set$Salary),
+             color = 'red') +
+  geom_line(aes(x = training_set$YearsExperience, y = predict(regressor, newdata = training_set)),
+            color = 'blue') +
+  ggtitle('Salary vs Experience (Test set)') +
+  xlab('Years of expereince') +
+  ylab('Salary')
