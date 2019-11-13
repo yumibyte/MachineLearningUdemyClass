f41307e8f7c3dcfa7a50214ff2dd1f32a17f5242
Yumibyte <ashraigosa@gmail.com>
2019-11-12 21:50:30 -0800

    visualizing linear regression results

--- my_polynomial_regression.py
+++ my_polynomial_regression.py
@@ -17,13 +17,12 @@ import pandas as pd
 
 # Importing the dataset
 dataset = pd.read_csv('Position_Salaries.csv')
-X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 3].values
+X = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 2].values
 
 # Splitting the dataset into the Training set and Test set
-from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
-
+"""from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
 # Feature Scaling
 """from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
@@ -43,3 +42,13 @@ poly_reg = PolynomialFeatures(degree = 2)
 X_poly = poly_reg.fit_transform(X)
 lin_reg_2 = LinearRegression()
 lin_reg_2.fit(X_poly, y)
+
+
+# Visualizing the Linear Regression results
+plt.scatter(X, y, color= 'red')
+plt.plot(X, lin_reg.predict(X), color = 'blue')
+plt.title('truth or bluff Linear Regression')
+plt.xlabel('position level')
+plt.ylabel('Salary')
+plt.show()
+# Visualizing the Polynomial Regression results
